989e969b6c3228499b96298a65f419381790c916
Mariana Teixeira <teixeira.marianareis@gmail.com>
2026-01-05 18:02:53 +0000

    refactor: delete all code
    + feat: add seek steering behaviour
    + doc: add comments of study resources

--- boids-demo/Assets/Scripts/DemoManager.cs
+++ boids-demo/Assets/Scripts/DemoManager.cs
@@ -1,102 +1,117 @@
-using Boids.Jobs;
+using System;
+using System.Collections;
 using Unity.Collections;
-using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.Jobs;
+using Random = UnityEngine.Random;
 
-namespace Boids
+namespace Demo.Boids
 {
+    // accessing CPU cache (L1 and L2) is fast
+    // accessing RAM memory is slow
+    // cache misses are expensive and should be avoided
+    // cache misses happen when the CPU must access memory from RAM
+    // to avoid cache misses flatten your data (arrays)
+    // to avoid cache misses use immutable data when possible
+
     public class DemoManager : MonoBehaviour
     {
-        [SerializeField] private Settings m_settings;
+        [SerializeField] private Transform m_target;
+        [SerializeField] private float m_targetRadius;
+        private bool m_isNearTarget;
         
-        private NativeParallelMultiHashMap<int, int> m_spatialMap;
-        private TransformAccessArray m_transformArray;
+        // m_count is located on the heap as a value type (4 bytes)
+        // since m_count is located on the heap, it requires the CPU to fetch from the RAM
+        // the CPU fetches a cache line from RAM, which makes flat data (such as arrays) better for performance
+        [SerializeField, ReadOnly] private int m_count;
+        
+        // m_prefab is located on the heap as a reference type
+        // since m_count is located on the heap, it requires the CPU to fetch from the RAM (cache miss)
+        // furthermore, the RAM does not store a value that can be fetched, but a address of the actual data
+        [SerializeField] private GameObject m_prefab;
+    
+        // m_bodies is a standard reference type array that contains a pointer to data
+        private Rigidbody[] m_bodies;
+
+        [SerializeField] private float m_worldRadius;
+
+        [SerializeField] private float m_maxSpeed;
+    
+        // m_positions is a value type array that contains a pointer to data
+        // a native array allows programmers to do
+        // 1) safety multithreading (ensuring multiple threads don't write to the same index)
+        // 2) burst compilation
+        private NativeArray<float3> m_velocity;
         private NativeArray<float3> m_positions;
-        private NativeArray<float3> m_velocities;
-        private NativeArray<float3> m_steerings;
-
-        private JobHandle m_jobHandle;
-
+        private NativeArray<float3> m_steering;
+    
         private void Awake()
         {
-            m_spatialMap = new NativeParallelMultiHashMap<int, int>(m_settings.BoidCount, Allocator.Persistent);
-            m_positions = new NativeArray<float3>(m_settings.BoidCount, Allocator.Persistent);
-            m_velocities = new NativeArray<float3>(m_settings.BoidCount, Allocator.Persistent);
-            m_steerings = new NativeArray<float3>(m_settings.BoidCount, Allocator.Persistent);
-            
-            m_settings.VisionThreshold = math.cos(m_settings.VisionAngle * 0.5f * math.TORADIANS);
+            // allocator defines how long that memory is valid
+            m_bodies = new Rigidbody[m_count];
+            m_velocity = new NativeArray<float3>(m_count, Allocator.Persistent);
+            m_positions = new NativeArray<float3>(m_count, Allocator.Persistent);
+            m_steering = new NativeArray<float3>(m_count, Allocator.Persistent);
         }
-
+    
         private void Start()
         {
-            Transform[] transforms = new Transform[m_settings.BoidCount];
-            for (int i = 0; i < m_settings.BoidCount; i++)
+            for (int i = 0; i < m_count; i++)
             {
-                Transform boidTransform = Spawner.Spawn(m_settings.GridCenter, m_settings.GridRadius, m_settings.BoidPrefab);
-                transforms[i] = boidTransform;
-                m_positions[i] = boidTransform.position;
-                m_velocities[i] = boidTransform.forward * m_settings.MinSpeed;
+                Vector3 randomPosition = Random.insideUnitSphere * Random.Range(0.0f, m_worldRadius);
+                GameObject boid = Instantiate(m_prefab, randomPosition, Random.rotation);
+                Rigidbody body = boid.GetComponent<Rigidbody>();
+                m_bodies[i] = body;
+                m_positions[i] = body.position;
+                m_velocity[i] = Random.Range(1.0f, m_maxSpeed);
             }
-            m_transformArray = new TransformAccessArray(transforms);
         }
 
         private void Update()
         {
-            m_spatialMap.Clear();
-
-            SpatialMapJob spatialMapJob = new SpatialMapJob
-            {
-                Positions = m_positions,
-                CellSize = m_settings.CellRadius,
-                SpatialMap = m_spatialMap.AsParallelWriter()
-            };
-            JobHandle spatialHandle = spatialMapJob.Schedule(m_settings.BoidCount, 64);
-            
-            BoidSteerJob steerJob = new BoidSteerJob
-            {
-                SpatialMap = m_spatialMap,
-                GridCenter = m_settings.GridCenter,
-                GridRadius = m_settings.GridRadius,
-                CellSize = m_settings.CellRadius,
-                VisionRadius = m_settings.VisionRadius,
-                VisionThreshold = m_settings.VisionThreshold,
-                SpringWeight = m_settings.SpringWeight,
-                AlignmentWeight = m_settings.AlignmentWeight,
-                SeparationWeight = m_settings.SeparationWeight,
-                CohesionWeight = m_settings.CohesionWeight,
-                Positions = m_positions,
-                Velocities = m_velocities,
-                Steerings = m_steerings
-            };
-            JobHandle steerHandle = steerJob.Schedule(m_settings.BoidCount, 32, spatialHandle);
-            
-            BoidMoveJob moveJob = new BoidMoveJob
-            {
-                DeltaTime = Time.deltaTime,
-                MinSpeed = m_settings.MinSpeed,
-                MaxSpeed = m_settings.MaxSpeed,
-                RotationSpeed = m_settings.RotationSpeed,
-                Positions = m_positions,
-                Velocities = m_velocities,
-                Steerings = m_steerings
-            };
-            m_jobHandle = moveJob.Schedule(m_transformArray, steerHandle);
+            if (!m_isNearTarget) return;
+            m_target.position = Random.insideUnitSphere * Random.Range(0.0f, m_worldRadius);
+            m_isNearTarget = false;
         }
 
-        private void LateUpdate()
+        private void FixedUpdate()
         {
-            m_jobHandle.Complete();
+            for (int i = 0; i < m_count; i++)
+            {
+                Rigidbody body = m_bodies[i];
+
+                m_positions[i] = body.position;
+                m_velocity[i] = body.linearVelocity;
+                float3 targetPosition = m_target.position;
+
+                if (math.lengthsq(m_positions[i] - targetPosition) < m_targetRadius * m_targetRadius)
+                {
+                    m_isNearTarget = true;
+                }
+
+                float3 desiredVector = targetPosition - m_positions[i];
+                float3 desiredVelocity = math.normalize(desiredVector) * m_maxSpeed;
+                m_steering[i] = desiredVelocity - m_velocity[i];
+
+                body.AddForce(m_steering[i], ForceMode.Force);
+            }
         }
 
         private void OnDestroy()
         {
-            m_spatialMap.Dispose();
-            m_transformArray.Dispose();
+            m_velocity.Dispose();
             m_positions.Dispose();
-            m_velocities.Dispose();
-            m_steerings.Dispose();
+            m_steering.Dispose();
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(Vector3.zero, m_worldRadius);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(m_target.position, m_targetRadius * 0.2f);
+            Gizmos.DrawWireSphere(m_target.position, m_targetRadius);
         }
     }
 }
